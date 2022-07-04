0e163e96f0dc25300e7a7821b896f3b844df74a4
Medstoun <kirillmyagchilov@gmail.com>
2022-07-04 19:47:47 +0300

    Done

---
+++ constants.py
@@ -0,0 +1,13 @@
+PWD_HASH_SALT = b'secret here'
+PWD_HASH_ITERATIONS = 100_000
+
+USER_KEYS = {'email', 'password'}
+TOKEN_KEYS = {'email', 'exp', 'id', 'password', 'name', 'surname', 'favourite_genre'}
+MOVIE_KEYS = {'title', 'description', 'trailer', 'year', 'rating', 'genre_id', 'director_id'}
+DIRECTOR_KEYS = {'name'}
+GENRE_KEYS = {'name'}
+
+SECRET = 's3cR$eT'
+ALGO = 'HS256'
+
+LIMIT = 12
---
+++ dao/movie.py
@@ -0,0 +1,22 @@
+from sqlalchemy import desc
+
+from dao.model.movie import Movie
+from utils import get_pagination
+
+
+class MovieDAO:
+    def __init__(self, session):
+        self.session = session
+
+    def get_one(self, uid):
+        return self.session.query(Movie).get(uid)
+
+    def get_all(self, page, status):
+        select = self.session.query(Movie)
+
+        offs, lim = get_pagination(Movie, page)
+
+        if status == 'new':
+            select = select.order_by(desc(Movie.year))
+
+        return select.limit(lim).offset(offs).all()
